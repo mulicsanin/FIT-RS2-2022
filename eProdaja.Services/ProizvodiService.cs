189c5bbff317db2c9b80543d6bf1e0697beb9b95
Mulić Sanin <mulic.sanin@gmail.com>
2022-03-12 22:17:02 +0100

    V1-2022-03-12

---
+++ eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodiService : IProizvodiService
+    {
+        public IEnumerable<Proizvodi> Get()
+        {
+            return new List<Proizvodi>() { new Proizvodi() { Id = 1, Name = "Laptop"}, new Proizvodi() { Id=2, Name="Mis" } };
+        }
+    }
+}
---
+++ eProdaja/Controllers/ProizvodiController.cs
@@ -0,0 +1,23 @@
+﻿using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodiController : ControllerBase
+    {
+        private readonly IProizvodiService _proizvodiService;
+
+        public ProizvodiController(IProizvodiService proizvodiService)
+        {
+            _proizvodiService = proizvodiService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Proizvodi> Get()
+        { 
+            return _proizvodiService.Get();
+        }
+    }
+}
